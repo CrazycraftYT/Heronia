5dfc4d34b2d7d3daf4889dcd50097a49578db864
CrazycraftYT <44378485+CrazycraftYT@users.noreply.github.com>
2018-10-23 20:32:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -31,8 +31,8 @@ bot.on('message', message => {
         var embed = new Discord.RichEmbed()
             .setTitle("Voici l'IP du serveur :")
             .setDescription(" -=- ")
-            .addField("*mc.heronia.fr*", true)
-            .addField("Si cela ne fonctionne pas, contactez un membre du Staff.", true)
+            .addField("*mc.heronia.fr*", " ", true)
+            .addField("Si cela ne fonctionne pas, contactez un membre du Staff.", " ", true)
             .setColor(0x0000FF)
             .setFooter("by Heronia » Crazy")
         message.channel.sendEmbed(embed);
@@ -42,8 +42,8 @@ bot.on('message', message => {
         var embed = new Discord.RichEmbed()
             .setTitle("Voici le forum du serveur :")
             .setDescription(" -=- ")
-            .addField("*heronia.fr*", true)
-            .addField("Si cela ne fonctionne pas, contactez un membre du Staff.", true)
+            .addField("*heronia.fr*", " ", true)
+            .addField("Si cela ne fonctionne pas, contactez un membre du Staff.", " ", true)
             .setColor(0x0000FF)
             .setFooter("by Heronia » Crazy")
         message.channel.sendEmbed(embed);
