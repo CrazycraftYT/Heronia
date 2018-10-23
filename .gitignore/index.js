8639e0a44842a25b8c4fc35dd3b5a192585d0e78
CrazycraftYT <44378485+CrazycraftYT@users.noreply.github.com>
2018-10-23 19:02:04 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,7 +19,7 @@ bot.on('message', message => {
     if (message.content === prefix + "help"){
         var embed = new Discord.RichEmbed()
             .setTitle("Heronia Commande Discord")
-            .setDescription("AAA")
+            .setDescription("Liste des commandes du discord")
             .addField("» Serveur «", "!ip » Permet d'afficher l'IP du serveur", true)
             .addField("» Forum «", "!site » Permet d'obtenir le lien du forum d'Heronia", true)
             .setColor(0xFF8000)
