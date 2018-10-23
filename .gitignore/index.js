8955ef3c3fde47b87af3b5170b71a4bf0565c26c
CrazycraftYT <44378485+CrazycraftYT@users.noreply.github.com>
2018-10-23 17:53:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -18,22 +18,12 @@ bot.on("message", async function(message) {
 
     switch(args[0].toLowerCase()) {
         case "help":
-        message.channel.send("", {
-            embed: {
-                color: 0xFF0000,
-                author: message.author.name,
-                title: '',
-                fields: [{
-                    name: "Commande Heronia Discord",
-                    value: "!ip » Permet d'afficher l'IP du serveur d'Heronia",
-                    value: "!site » Permet d'afficher le lien du site du serveur Heronia",
-                    value: "!twitter » Permet d'afficher le lien du twitter du serveur d'Heronia",
-                    inline: false
-                }],
-                footer: {
-                    footer: "Partager ce lien à tous vos amis !",
-                },
-            }
+        var embed = new Discord.RichEmbed()
+            .setTitle("Heronia Commande Discord")
+            .addField("!help", "Page d'aide")
+            .addField("Embed1", "Embed1")
+            .setColor(0xFF8000)
+            .setFooter("Bon moment")
         });
         break;
     }
