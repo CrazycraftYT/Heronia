2c1d942f4b3612cc1012b8ad9034bc3b4b0ad95e
CrazycraftYT <44378485+CrazycraftYT@users.noreply.github.com>
2018-10-24 11:33:53 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -62,56 +62,6 @@ bot.on('message', message => {
         message.channel.sendEmbed(embed);
     }
 
-    const Discord = require('discord.js')
-const Bot = new Discord.Client()
-
-var token_login = "Le token se situe dans la page developpeurs, cliquez sur "click to reveal" et copiez-collez le ici !"
-var prefix = ":"
-
-Bot.on('ready', () => {
-
-console.log("Bot prêt");
-});
-
-Bot.on("message", async message => {
-
-  if(command === prefix + "mute"){
-
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'avez pas les droits pour muter un utilisateur !");
-
-    let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-    if(!toMute) return message.channel.send("Merci d'entrer un utilisateur !");
-    let role = message.guild.roles.find(r => r.name === "Utilisateurs mutés");
-    if(!role){
-      try {
-        role = await message.guild.createRole({
-          name: "Utilisateurs mutés",
-          color:"#000000",
-          permissions:[]
-        });
-
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(role, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
-        });
-      } catch (e) {
-        console.log(e.stack)
-      }
-    }
-
-    if(toMute.roles.has(role.id)) return message.channel.send('Cet utilisateur est déjà muté !');
-
-    await(toMute.addRole(role));
-    message.channel.send("Je l'ai muté !");
-
-    return;
-  }
-
-});
-    
-
 });
 
 bot.login(process.env.TOKEN);
