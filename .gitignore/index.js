917e1fa94ac10f69bd7668211b5d8551e7d20c7f
CrazycraftYT <44378485+CrazycraftYT@users.noreply.github.com>
2018-12-01 14:49:51 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -6,7 +6,7 @@ var prefix = "!";
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("Maintenance en cours - Crazy");
+    bot.user.setGame("Heronia.fr | !help");
     console.log("Le bot a bien ete connecte");
 });
 
